204d3d2656c1750e7c778a11530b7072e3f7c8d2
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2023-07-23 00:34:18 -0700

    yeah

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,23 @@
-﻿using ChessChallenge.API;
-
+﻿
+using System;
+using ChessChallenge.API;
+using System.Collections;
+using System.Diagnostics;
+//TODO: add the ability to en passant whenever possible, for teh lulz 
 public class MyBot : IChessBot
 {
+    //comment
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        bool MoveIsGood(Board board, Move move)
+        {
+            board.GetLegalMoves();
+            if (move.IsEnPassant)
+            {
+                MakeMove(Move move);
+                return 
+            }
+        }
+    
     }
 }
