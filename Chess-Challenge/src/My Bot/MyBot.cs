bd14fd8ed58531ac7fcd0b03d280b92edd761bcf
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2023-07-23 01:19:37 -0700

    yeah

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,19 +3,30 @@ using System;
 using ChessChallenge.API;
 using System.Collections;
 using System.Diagnostics;
-//TODO: add the ability to en passant whenever possible, for teh lulz 
+using System.Transactions;
+
+//TODO: do random moves, UNLESS there is an oppurtunity for en passant(the only good move)
+//TODO: maybe also implment racks kia's idea of only moving pawns furthest??
 public class MyBot : IChessBot
 {
     //comment
     public Move Think(Board board, Timer timer)
     {
+        Random rng = new();
+        Move[] allMoves = board.GetLegalMoves();
+        
+        
         bool MoveIsGood(Board board, Move move)
         {
             board.GetLegalMoves();
             if (move.IsEnPassant)
             {
-                MakeMove(Move move);
-                return 
+                ;
+            }
+            else
+
+            {
+                Random rng = new();
             }
         }
     
